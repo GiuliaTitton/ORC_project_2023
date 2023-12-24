1d78dbf8dfe512dad5cd102dfb8ef4e99f9c4bf9
GiuliaTitton <68745997+GiuliaTitton@users.noreply.github.com>
2023-12-24 19:52:11 +0100

    caricato critic modificato e immagini risultati test

--- A3/critic.py
+++ A3/critic.py
@@ -20,9 +20,9 @@ def tf2np(y):
 
 def get_critic(nx):
     ''' Create the neural network to represent the Q function '''
-    
+    weight_decay = 0.0009676325797855587
     # OPTION 1
-    
+    '''
     inputs = layers.Input(shape=(nx,1))
     # Add normalization layer
     normalized_inputs = layers.LayerNormalization()(inputs)
@@ -31,7 +31,7 @@ def get_critic(nx):
     state_out3 = layers.Dense(64, activation="relu")(state_out2) 
     state_out4 = layers.Dense(64, activation="relu")(state_out3)
     outputs = layers.Dense(1)(state_out4) 
-    
+    '''
     '''
     #OPTION 2
     inputs = layers.Input(shape=(nx,1))
@@ -41,7 +41,7 @@ def get_critic(nx):
     outputs = layers.Dense(1)(state_out2)
     '''
     
-    '''
+    
     #OPTION 3   -> migliore finora con batch di 15
     inputs = layers.Input(shape=(nx,1))
     normalized_inputs = layers.LayerNormalization()(inputs)
@@ -49,16 +49,15 @@ def get_critic(nx):
     state_out2 = layers.Dense(8, activation="relu")(state_out1) 
     state_out3 = layers.Dense(8, activation="relu")(state_out2)   
     outputs = layers.Dense(1)(state_out3)
-    '''
+    
     #OPTION 4 -> suggerimento di optuna
     
     '''
-    weight_decay = 0.0009676325797855587
     inputs = layers.Input(shape=(nx,1))
     normalized_inputs = layers.LayerNormalization()(inputs)
-    state_out1 = layers.Dense(18, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(weight_decay))(normalized_inputs) 
-    state_out2 = layers.Dense(43, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(weight_decay))(state_out1) 
-    state_out3 = layers.Dense(7, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(weight_decay))(state_out2) 
+    state_out1 = layers.Dense(16, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(weight_decay))(normalized_inputs) 
+    state_out2 = layers.Dense(8, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(weight_decay))(state_out1) 
+    state_out3 = layers.Dense(8, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(weight_decay))(state_out2) 
     outputs = layers.Dense(1)(state_out3) 
     '''
     
@@ -84,7 +83,7 @@ def update(x_batch, target_values):
     optimizer.apply_gradients(zip(V_grad, V.trainable_variables))
     train_acc_metric.update_state(target_values, V_value)  
      
-    return V_loss
+    return V_loss, V_value
 
 def val_step(x_batch, V_batch):
     val_logits = V(x_batch, training=False)
@@ -103,15 +102,16 @@ V.summary()
 optimizer = tf.keras.optimizers.Adam(VALUE_LEARNING_RATE)
 
 # Instantiate a loss function.
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+loss_fn = tf.keras.losses.MeanSquaredError()
+#loss_fn = tf.math.reduce_mean(tf.math.square(target_values - V_value)) 
 
 # Prepare the metrics.
-train_acc_metric = tf.keras.metrics.SparseCategoricalAccuracy()
-val_acc_metric = tf.keras.metrics.SparseCategoricalAccuracy()
+train_acc_metric = tf.keras.metrics.MeanSquaredError()
+val_acc_metric = tf.keras.metrics.MeanSquaredError()
 
 #prepare the training dataset
-num_epochs = 26
-batch_size = 128
+num_epochs = 100
+batch_size = 25
 
 #x_train = data['x_init']
 #V = data['V']
@@ -150,25 +150,31 @@ print("V shape:", V_train.shape)
 
 print("Starting training loop")
 tot = []
+tot_loss = []
+tot_pe = []
+total_time_for_training = 0
+#---------------TRAINING---------------#
 # Training loop
 for epoch in range(num_epochs):
     epoch_losses = []  # To store losses for each batch within the epoch
     print(f"\nStart of epoch {epoch+1}" )
     start_time = time.time()
+    pe_tot = []
 
     # for each epoch open a for loop on the set of data 
     for i, (x_batch_train, V_batch_train) in enumerate(train_dataset):
         x_batch = np2tf(x_batch_train)  # Convert numpy to tensorflow
         V_batch = np2tf(V_batch_train)
-        V_loss = update(x_batch, V_batch)
+        V_loss, V_value = update(x_batch, V_batch)
         epoch_losses.append(V_loss.numpy())  # Store the loss for this batch
-    
+        train_acc_metric.update_state(V_batch, V_value)
+         
+        
+    print(f"V_loss  = {V_loss}")
+    tot_loss.append(V_loss)
     # Display metrics at the end of each epoch.
     train_acc = train_acc_metric.result()
-    print("Training acc over epoch: %.4f" % (float(train_acc),))
-
-    # Reset training metrics at the end of each epoch
-    train_acc_metric.reset_states()
+    #print("Training acc over epoch: %.4f" % (float(train_acc),))
     
     # Run a validation loop at the end of each epoch.
     print(f"Validation of epoch {epoch+1}")
@@ -176,35 +182,48 @@ for epoch in range(num_epochs):
         x_batch = np2tf(x_batch_val)  # Convert numpy to tensorflow
         V_batch = np2tf(V_batch_val)
         val_step(x_batch, V_batch)
+        val_acc_metric.update_state(V_batch, V(x_batch))
     val_acc = val_acc_metric.result()
-    val_acc_metric.reset_states()
     print("Validation acc: %.4f" % (float(val_acc),))
-    print("Time taken: %.2fs" % (time.time() - start_time))
+    # Reset metrics at the end of each epoch
+    train_acc_metric.reset_states()
+    val_acc_metric.reset_states()
+    
+    time_taken = (time.time() - start_time)
+    print("Time taken: %.2fs" % time_taken)
+    total_time_for_training += time_taken
     
     # Compute epoch loss as the average of all batch losses in this epoch
     epoch_loss = np.mean(epoch_losses)
     # Print epoch information
     print(f"Epoch {epoch+1}/{num_epochs}, Loss: {epoch_loss}")
     tot.append(epoch_loss)
+
+print(f"Total time taken for training: {total_time_for_training}")
     
 # Plotting the data of losses during training
 plt.plot(tot)
 plt.xlabel('# epoch')  
 plt.ylabel('Loss')  
-plt.title('Loss during training')     
+plt.title('EPOCH Loss during training')     
 plt.grid(True)   
-plt.show()      
+plt.show()     
 
-   
+plt.plot(tot_loss)
+plt.xlabel('')  
+plt.ylabel('Loss')  
+plt.title('V_loss during training (error in prediction)')     
+plt.grid(True)   
+plt.show()  
 
+#---------------TEST-----------------#
 # Testing loop to test the performance of the network with unseen data
 test_losses = []
 for x_batch_test, V_batch_test in test_dataset:
     x_batch = np2tf(x_batch_test)  # Convert numpy to tensorflow
     V_batch = np2tf(V_batch_test)
-    test_loss = update(x_batch, V_batch)
+    test_loss, test_value = update(x_batch, V_batch)
     test_losses.append(test_loss.numpy())
-
 # Compute test loss as the average of all test batch losses
 test_loss = np.mean(test_losses)
 print(f"Test Loss: {test_loss}")
