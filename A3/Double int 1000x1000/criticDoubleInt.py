da2f51697ed663b80b2f9bf6dc7921393a4bd001
GiuliaTitton <68745997+GiuliaTitton@users.noreply.github.com>
2024-01-04 21:16:38 +0100

    caricata cartella con file per 1000x1000
    E' stato testato fino all'ocp. il critic deve ancora essere trainato

---
+++ A3/Double int 1000x1000/criticDoubleInt.py
@@ -0,0 +1,140 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import time
+
+
+# Carica set di dati
+data = np.load('resultsDoubleInt.npz')
+x_data = data['x_init']
+velocity_data = data['v_init']
+print(f"x_data shape={x_data.shape}")
+print(f"velocity_data shape={velocity_data.shape}")
+x_grid, vel_grid = np.meshgrid(x_data, velocity_data)
+states_data = np.stack((x_grid, vel_grid), axis=-1)
+states_data = states_data.reshape((-1, 2, 1))
+V_data = data['V']
+V_data = np.array(V_data).reshape((-1, 1))
+
+states_tensor = tf.convert_to_tensor(states_data, dtype=tf.float32)
+V_tensor = tf.convert_to_tensor(V_data, dtype=tf.float32)
+
+# Crea dataset e mescola i dati
+dataset = tf.data.Dataset.from_tensor_slices((states_tensor, V_tensor))
+dataset_shuffle = dataset.shuffle(buffer_size=10500)
+#for element in dataset_shuffle.as_numpy_iterator():
+#  print(element)
+
+print("Creating train,validation and test dataset...")
+# Crea train, test e validation dataset 
+train_dataset = dataset_shuffle.take(700000)
+
+start_index_test = 850000
+test_dataset = dataset_shuffle.skip(start_index_test).take(1500)
+
+start_index_val = 700000
+val_dataset = dataset_shuffle.skip(start_index_val).take(1500)
+
+# Estrai x_train e V_train come numpy array
+states_train = []
+V_train = []
+
+for state, V in train_dataset:
+    states_train.append(state.numpy())
+    V_train.append(V.numpy())
+
+states_train = np.array(states_train)
+V_train = np.array(V_train)
+
+# Estrai x_val e V_val come numpy array
+states_val = []
+V_val = []
+
+for state, V in val_dataset:
+    states_val.append(state.numpy())
+    V_val.append(V.numpy())
+
+states_val = np.array(states_val)
+V_val = np.array(V_val)
+
+# Estrai x_test e V_test come numpy array
+states_test = []
+V_test = []
+
+for state, V in test_dataset:
+    states_test.append(state.numpy())
+    V_test.append(V.numpy())
+
+states_test = np.array(states_test)
+V_test = np.array(V_test)
+
+print("Building model...")
+# Costruisci il modello
+nx = 2
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(nx, 1)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(64, activation='relu'),
+  tf.keras.layers.Dense(32, activation='relu'),
+  tf.keras.layers.Dense(32, activation='relu'),
+  tf.keras.layers.Dense(64, activation='relu'),
+  tf.keras.layers.Dense(1)
+])
+model.summary()
+
+#get starting time 
+start_time = time.time()
+
+print("Compiling model...")
+model.compile(optimizer=tf.keras.optimizers.Adam(),
+              loss='mse',  # Use mean squared error for regression
+              metrics=['mse'])  # Use mse for evaluation
+print("Model compiled successfully")
+
+# Allena e valuta il modello 
+EPOCHS = 20
+BATCH_SIZE = 32 
+
+print("Fitting model...")
+history = model.fit(states_train, V_train, epochs=EPOCHS, batch_size=BATCH_SIZE, validation_data=(states_val, V_val))
+
+print("Making predictions...")
+predictions = model.predict(states_test)
+#print("Predictions:", predictions)
+
+#get final time for training and testing
+final_time = time.time()
+
+#compute the elapsed time (= time taken for training critic network)
+elapsed_time = final_time - start_time
+print(f"Time taken for training critic network: {elapsed_time}")
+
+prediction_tot_dataset = model.predict(states_data)
+predictions_reshaped = prediction_tot_dataset.reshape(x_grid.shape)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+surf = ax.plot_surface(x_grid, vel_grid, predictions_reshaped, cmap='viridis')
+ax.set_xlabel('Initial position')
+ax.set_ylabel('Initial velocity')
+ax.set_zlabel('Predicted cost')
+ax.set_title('Critic predictions')
+fig.colorbar(surf, ax=ax, shrink=0.5, aspect=10)
+plt.show()
+
+V_data_reshaped = V_data.reshape(x_grid.shape)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+surf = ax.plot_surface(x_grid, vel_grid, V_data_reshaped, cmap='viridis')
+ax.set_xlabel('Initial position')
+ax.set_ylabel('Initial velocity')
+ax.set_zlabel('OCP cost')
+ax.set_title('OCP cost')
+fig.colorbar(surf, ax=ax, shrink=0.5, aspect=10)
+plt.show()
+
+#salva su file i valori predetti 
+np.savez('PredictionsForActor.npz', prediction_tot_dataset)
+
+# Salva il modello
+model.save('model_critic_doubleIntegrator')
---
+++ A3/Double int 1000x1000/ocpFromActor.py
@@ -0,0 +1,126 @@
+import numpy as np
+import casadi
+
+class OcpDoubleIntegrator:
+
+    def __init__(self, dt, w_u, u_min=None, u_max=None):
+        self.dt = dt
+        self.w_u = w_u
+        self.u_min = u_min
+        self.u_max = u_max
+
+    def solve(self, x_init,v_init, N, X_guess=None, U_guess=None):
+        self.opti = casadi.Opti()
+        self.x = self.opti.variable(N+1,2) #2 states for position and velocity
+        self.u = self.opti.variable(N)
+        x = self.x
+        u = self.u
+
+        if(X_guess is not None):
+            for i in range(N+1):
+                self.opti.set_initial(x[i,:], X_guess[i,:])
+        else:
+            for i in range(N+1):
+                self.opti.set_initial(x[i,:], [x_init, v_init])
+        if(U_guess is not None):
+            for i in range(N):
+                self.opti.set_initial(u[i], U_guess[i,:])
+
+        self.cost = 0
+        self.running_costs = [None,]*(N+1)
+        for i in range(N+1):
+            self.running_costs[i] = (x[i,0]-1.9)*(x[i,0]-1.0)*(x[i,0]-0.6)*(x[i,0]+0.5)*(x[i,0]+1.2)*(x[i,0]+2.1)
+            if(i<N):
+                self.running_costs[i] += self.w_u * u[i]*u[i]
+            self.cost += self.running_costs[i]
+        self.opti.minimize(self.cost)
+
+        for i in range(N):
+            #self.opti.subject_to( x[i+1]==x[i] + self.dt*u[i] )
+            self.opti.subject_to(x[i+1,0] ==x[i,0] + self.dt * x[i,1] + 0.5 * self.dt**2 * u[i])  # Position update
+            self.opti.subject_to(x[i+1,1] == x[i,1] + self.dt * u[i])
+        if self.u_min is not None and self.u_max is not None:
+            for i in range(N):
+                self.opti.subject_to(self.opti.bounded(self.u_min, u[i], self.u_max))
+        self.opti.subject_to(x[0, 0] == x_init)
+        self.opti.subject_to(x[0, 1] == v_init)
+
+        # s_opts = {"max_iter": 100}
+        opts = {'ipopt.print_level': 0, 'print_time': 0, 'ipopt.sb': 'yes'}
+        self.opti.solver("ipopt", opts) #, s_opts)
+
+        return self.opti.solve()
+
+
+if __name__=="__main__":
+    import matplotlib.pyplot as plt
+    #load file with actor results
+    data = np.load('ActorResults.npz')
+    control_from_actor=data['arr_0']
+    print(control_from_actor)
+    
+    N = 10          # horizon size
+    dt = 0.1        # time step
+    w_u = 0.5
+    u_min = -5      # min control input
+    u_max = 5       # max control input
+    N_OCP = 100     # sqrt of number of OCPs (training episodes)
+    plot = 1        # plot states-cost
+
+    #compute new OCP
+    ocp = OcpDoubleIntegrator(dt, w_u, u_min, u_max)
+
+    # solve OCP starting from different initial states, based on actor predictions
+    x_init = np.linspace(-2.2, 2.0, N_OCP) # array of initial states
+    v_init = np.linspace(-1.0, 1.0, N_OCP) # array of initial velocities
+    V = np.zeros((N_OCP, N_OCP))                    # array of V(x0) for each initial state
+    u_optimal = np.zeros((N_OCP, N_OCP))
+    for i in range(0, N_OCP):
+        for j in range(0, N_OCP):
+            sol = ocp.solve(x_init[i],v_init[j], N, None, control_from_actor)
+            V[i,j] = sol.value(ocp.cost)
+            # , [ocp.x==[x_init[i], v_init[j]]]
+            u_optimal[i, j] = sol.value(ocp.u)[0]
+            print("OCP number ", i, "\n Initial position: ", sol.value(ocp.x[0,0]), "Initial velocity: ", sol.value(ocp.x[0,1]),"\n Cost: ", V[i,j])
+    print(f"Array of costs from OCP: {V}")
+    print(f"Mean cost of OCP: {np.mean(V)}")
+    if plot:
+
+        # Create a 3D plot
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        X, Y = np.meshgrid(x_init, v_init)
+        ax.plot_surface(X, Y, V, cmap='viridis')
+        ax.set_xlabel('Initial position')
+        ax.set_ylabel('Initial velocity')
+        ax.set_zlabel('Cost')
+        ax.set_title('Cost of the OCPs')
+        plt.show()
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        X, Y = np.meshgrid(x_init, v_init)
+        u_opt_reshaped = u_optimal.reshape(X.shape)
+        ax.plot_surface(X, Y, u_opt_reshaped, cmap='viridis')
+        ax.set_xlabel('Initial position')
+        ax.set_ylabel('Initial velocity')
+        ax.set_zlabel('Optimal control')
+        ax.set_title('Controls of OCPs starting from different initial states')
+        plt.show()
+
+    
+    #x_init_values = x_init.tolist()
+    #V_values = V.tolist()
+    
+    # Saving data to a Numpy .npz file
+    np.savez('ocpAfterActor.npz', x_init=x_init, v_init=v_init, V=V)
+
+    # Loading data from a Numpy .npz file
+    data = np.load('ocpAfterActor.npz')
+    x_init_values = data['x_init']
+    v_init_values = data['v_init']
+    V_values = data['V']
+    
+    print("INITIAL STATES: ", x_init)
+    print("INITIAL VELOCITITES: ", v_init)
+    print("COSTS: ", V)
